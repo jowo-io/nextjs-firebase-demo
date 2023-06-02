e603341674b7d2495e91f34ca9c3ddcc29c1bdd9
jowo <me@jowo.io>
2023-06-02 15:01:48 +0200

    remove dynamic data from account page

--- app/(webapp)/account/page.tsx
+++ app/(webapp)/account/page.tsx
@@ -1,18 +1,5 @@
-import { getServerAuth } from "@/utils/firebase/admin";
-
-async function getData() {
-  const { user } = await getServerAuth();
-
-  return {
-    user,
-  };
-}
-
 export default async function Page() {
-  const data = await getData();
   return (
-    <p className="text-black dark:text-white">
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </p>
+    <p className="text-black dark:text-white">Static rendered account page</p>
   );
 }
